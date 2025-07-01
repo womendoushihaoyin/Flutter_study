e1ac9fe1ac02ed9e843e9e74e0f35742ee5a6040
womendoushihaoyin <xyliu_leo@163.com>
2025-07-01 09:31:14 +0800

    自定义小部件

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 
-void main() {
-  runApp(
-    Center(
+// 箭头函数
+main() => runApp(App());
+
+
+// 封装组件
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Center(
       child: Text(
         "Hello",
         textDirection: TextDirection.rtl,
       ),
-    ),
-  );
+    );
+  }
 }
