fa9c42f27b17a58b0bf16520dc269df304d7d3a8
womendoushihaoyin <xyliu_leo@163.com>
2025-07-02 21:42:46 +0800

    2.6,7,8 list

---
+++ flutter_study/lib/Model/post.dart
@@ -0,0 +1,34 @@
+class Post{
+  Post({
+    this.title = '',
+    this.author = '',
+    this.imageUrl = '',
+    this.description = '',
+  });
+
+  final String title;
+  final String author;
+  final String imageUrl;
+  final String description;
+}
+
+final posts = [
+  Post(
+    title: 'Candy Shop',
+    author: 'Mohamed Chahin',
+    description: 'Esse ut nulla velit reprehenderit veniam sint nostrud nulla exercitation ipsum. Officia deserunt aliquip aliquip excepteur eiusmod dolor. Elit amet ipsum labore sint occaecat dolore tempor officia irure voluptate ad. Veniam laboris deserunt aute excepteur sit deserunt dolor esse dolor velit sint nulla anim ut. Reprehenderit voluptate adipisicing culpa magna ea nulla ullamco consectetur. Cupidatat adipisicing consequat adipisicing sit consectetur dolor occaecat.',
+    imageUrl: 'https://resources.ninghao.org/images/candy-shop.jpg',
+  ),
+  Post(
+    title: 'Childhood in a picture',
+    author: 'Mohamed Chahin',
+    description: 'Esse ut nulla velit reprehenderit veniam sint nostrud nulla exercitation ipsum. Officia deserunt aliquip aliquip excepteur eiusmod dolor. Elit amet ipsum labore sint occaecat dolore tempor officia irure voluptate ad. Veniam laboris deserunt aute excepteur sit deserunt dolor esse dolor velit sint nulla anim ut. Reprehenderit voluptate adipisicing culpa magna ea nulla ullamco consectetur. Cupidatat adipisicing consequat adipisicing sit consectetur dolor occaecat.',
+    imageUrl: 'https://resources.ninghao.org/images/candy-shop.jpg',
+  ),
+  Post(
+    title: 'The old oil painting',
+    author: 'Mohamed Chahin',
+    description: 'Esse ut nulla velit reprehenderit veniam sint nostrud nulla exercitation ipsum. Officia deserunt aliquip aliquip excepteur eiusmod dolor. Elit amet ipsum labore sint occaecat dolore tempor officia irure voluptate ad. Veniam laboris deserunt aute excepteur sit deserunt dolor esse dolor velit sint nulla anim ut. Reprehenderit voluptate adipisicing culpa magna ea nulla ullamco consectetur. Cupidatat adipisicing consequat adipisicing sit consectetur dolor occaecat.',
+    imageUrl: 'https://resources.ninghao.org/images/candy-shop.jpg',
+  ),
+];
--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -1,25 +1,73 @@
 import 'package:flutter/material.dart';
+import 'Model/post.dart';
+import 'dart:io';
 
-
-
-main() => runApp(App());
-
-
-// 40号字，黄色，加粗
-
-class App extends StatelessWidget{
+class MyHttpOverrides extends HttpOverrides {
   @override
-  Widget build(BuildContext context){
-    return Center(
-      child: Text(
-        "Hello",
-        textDirection: TextDirection.rtl,
-        style: TextStyle(
-          fontSize: 40,
-          color: Colors.yellow,
-          fontWeight: FontWeight.bold,
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+     ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+main() {
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Hello(),
+      title: "xiaoye",
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        primaryColor: Colors.yellow,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.yellow,
+          foregroundColor: Colors.black,
         ),
       ),
+    );
+  }
+}
+
+class Hello extends StatelessWidget {
+  Widget _itemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          Image.network(
+            posts[index].imageUrl,
+            errorBuilder: (context, error, stackTrace) => Icon(Icons.error),
+          ),
+          SizedBox(height: 16),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.labelMedium,
+          ),
+          SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("lxy-code"), centerTitle: true, elevation: 10),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: _itemBuilder,
+      ),
+      backgroundColor: Colors.grey[100],
     );
   }
 }
