cc8abf10e8d90dbc330288a803964f00f46a649e
womendoushihaoyin <xyliu_leo@163.com>
2025-07-09 10:49:28 +0800

    4.8 BoxDecoration 边框

--- flutter_study/lib/Demo/basic_demo.dart
+++ flutter_study/lib/Demo/basic_demo.dart
@@ -23,6 +23,18 @@ class BasicDemo extends StatelessWidget {
             height: 90.0,
             decoration: BoxDecoration(
               color: Color.fromRGBO(3, 54, 255, 1.0),
+              // border: Border(
+              //   top: BorderSide(
+              //     color: Colors.indigoAccent,
+              //     width: 3.0,
+              //     style: BorderStyle.solid,
+              //   )
+              // )
+              border: Border.all(
+                color: Colors.indigoAccent,
+                width: 3.0,
+                style: BorderStyle.solid,
+              )
             ),
             child: Icon(Icons.pool, size: 32.0, color: Colors.white),
           ),
@@ -35,6 +47,19 @@ class BasicDemo extends StatelessWidget {
 
 /*
 
-decoration属性里面的颜色和外面的Color不能同时存在
+BoxDicoration(
+  border: Border(
+    top: BorderSide(
+      color,
+      width,
+      style
+    ),
+    bottom:
+    left:
+    right:
+  ),
+
+  border: Border.all()
+),
 
  */
