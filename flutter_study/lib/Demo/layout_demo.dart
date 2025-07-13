577fd5b3f0f94a188213026b1f473ea22dd83e7b
womendoushihaoyin <xyliu_leo@163.com>
2025-07-13 14:57:29 +0800

    5.8 sizedbox

--- flutter_study/lib/Demo/layout_demo.dart
+++ flutter_study/lib/Demo/layout_demo.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
 class LayoutDemo extends StatelessWidget {
-  @override
+  @override 
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
-        IconBadge(Icons.pool),
-        IconBadge(Icons.beach_access),
-        IconBadge(Icons.airplanemode_active),
+        SizedBox(
+          width: 200.0,
+          height: 300.0,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            ),
+            child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0),
+          ),
+        ),
+
+        SizedBox(
+          height: 32.0,
+        ),
+
+        SizedBox(
+          width: 100.0,
+          height: 100.0,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            ),
+            child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0),
+          ),
+        ),
+
+      
       ],
     );
   }
